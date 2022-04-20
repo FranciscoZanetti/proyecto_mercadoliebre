66ce67855a9f3cd75874ce451ee63ca928f6858c
FranciscoZanetti <franciscozanetti123@gmail.com>
2022-04-20 12:49:49 -0300

    subo modificaciones para Heroku

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express=require("express");
 const app=express();
 const path=require("path");
 
-app.listen(3000, ()=>console.log("Server Running..."));
+// app.listen(3000, ()=>console.log("Server Running..."));
+app.listen(process.env.PORT || 3000, ()=>console.log("Server Running..."));
 app.use(express.static("public"));
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, "/views/home.html")));
 app.get("/register", (req,res) => res.sendFile(path.join(__dirname, "/views/register.html")));
